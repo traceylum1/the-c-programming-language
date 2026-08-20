ebadb330b74d304265ae1eb9887b7a7824f6376a
Tracey Lum <traceylum@gmail.com>
2026-08-19 22:39:36 -0700

    bug in detab

--- ch_1/1-20.c
+++ ch_1/1-20.c
@@ -8,7 +8,7 @@
 #define TAB_STOPS 8
 
 int get_line(char line[], int lim);
-int detab(char from[], char to[]);
+void detab(char from[], char to[], int len);
 
 int main() {
     int c, len;
@@ -16,27 +16,41 @@ int main() {
     char detabbed[MAXLINE];
 
     while ((len = get_line(line, MAXLINE)) > 0) {
-        detab(line, detabbed);
+        detab(line, detabbed, len);
         printf("%s", detabbed);
+        // printf("%s", line);
     }
-
+    return 0;
 }
 
 int get_line(char line[], int lim) {
     int i, c;
+    i = 0;
     
     while (i < lim && (c = getchar()) != '\n') {
-        line[i] = c;
-        i++;
+        line[i++] = c;
     }
     if (c == '\n') {
-        line[i] = c;
-        i++;
+        line[i++] = c;
     }
     line[i] = '\0';
     return i;
 }
 
-int detab(char from[], char to[]) {
+void detab(char from[], char to[], int len) {
+    int i, j, dist_from_col;
+    j = 0;
+
+    for (i = 0; i < len; i++) {
+        if (from[i] == '\t') {
+            dist_from_col = TAB_STOPS - (j % TAB_STOPS);
+            while (dist_from_col > 0) {
+                to[j++] = ' ';
+                dist_from_col--;
+            }
+        } else {
+            to[j++] = from[i];
+        }
+    }
     
 }
